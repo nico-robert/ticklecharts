3d7d7fd2985926fcb4415bd3990116fe812ea145
nico-robert <nicolas.robert.19000@gmail.com>
2022-02-22 17:45:26 +0100

    line step example

---
+++ examples/line/line_step.tcl
@@ -0,0 +1,30 @@
+lappend auto_path [file dirname [file dirname [file dirname [file dirname [file normalize [info script]]]]]]
+
+# source all.tcl
+if {[catch {package present ticklecharts}]} {package require ticklecharts}
+
+set chart [ticklecharts::chart new]
+
+$chart SetOptions -title   {text "Step Line"} \
+                  -tooltip {trigger "axis"} \
+                  -legend  {} \
+                  -grid    {left "3%" right "4%" bottom "3%" containLabel true}
+               
+$chart Xaxis -data [list {Mon Tue Wed Thu Fri Sat Sun}]
+$chart Yaxis
+$chart AddLineSeries -name "Step Start" \
+                     -step "start" \
+                     -data [list {120 132 101 134 90 230 210}]
+                     
+$chart AddLineSeries -name "Step Middle" \
+                     -step "middle" \
+                     -data [list {220 282 201 234 290 430 410}]            
+
+$chart AddLineSeries -name "Step End" \
+                     -step "end" \
+                     -data [list {450 432 401 454 590 530 510}]                        
+
+set fbasename [file rootname [file tail [info script]]]
+set dirname [file dirname [info script]]
+
+$chart render -outfile [file join $dirname $fbasename.html] -title $fbasename
