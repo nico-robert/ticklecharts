bf87772ec4ac32922b1c80a40d88b0a32e800c93
nico-robert <nicolas.robert.19000@gmail.com>
2022-04-07 19:56:18 +0200

    add `tree`chart examples

---
+++ examples/tree/tree_from_right_to_left.tcl
@@ -0,0 +1,70 @@
+proc getchild {value} {
+    global namecollaspe
+
+    if {![dict exists $value children]} {
+        return ""
+    }
+
+    foreach item [dict get $value children] {
+        if {[dict exists $item children]} {
+            if {[expr {[llength [dict get $item children]] % 2}]} {
+                if {[dict exists $item name]} {
+                    lappend namecollaspe [dict get $item name]
+                }
+            }
+        }
+        getchild $item
+    }
+}
+
+lappend auto_path [file dirname [file dirname [file dirname [file dirname [file normalize [info script]]]]]]
+
+# source all.tcl
+if {[catch {package present ticklecharts}]} {package require ticklecharts}
+
+try {
+    # https://wiki.tcl-lang.org/page/HTTPS
+    #
+    package require http 2
+    package require tls 1.7
+    package require json
+
+    http::register https 443 [list ::tls::socket -autoservername true]
+    set token [http::geturl https://raw.githubusercontent.com/apache/echarts-examples/gh-pages/public/data/asset/data/flare.json]
+
+    set htmldata [::http::data $token]
+    set datajson [json::json2dict $htmldata]
+
+    # find child and collaspe tree if necessary...
+    getchild $datajson
+
+    foreach line [split $htmldata "\n"] {
+        foreach valname $namecollaspe {
+            if {[string match "*$valname*" $line]} {
+                set line "$line \"collapsed\": true,"
+                break
+            }
+        }
+        lappend newdata $line
+    }
+
+    set chart [ticklecharts::chart new]
+
+    $chart SetOptions -tooltip {trigger "item" triggerOn "mousemove"}
+                
+    $chart AddTreeSeries -top "1%" -left "15%" -bottom "1%" -right "7%" -symbolSize 7 -orient "RL" \
+                         -label {position "right" verticalAlign "middle" align "left"} \
+                         -leaves {label {position "left" verticalAlign "middle" align "right"}} \
+                         -emphasis {focus "descendant"} \
+                         -expandAndCollapse "True" -animationDurationUpdate 750 -animationDuration 550 \
+                         -data [list [json::json2dict [join $newdata "\n"]]]
+
+    set fbasename [file rootname [file tail [info script]]]
+    set dirname [file dirname [info script]]
+
+    $chart Render -outfile [file join $dirname $fbasename.html] -title $fbasename -width 1500px -height 1500px
+
+} on error {result options} {
+    puts stderr "[info script] : $result"
+}
+
---
+++ examples/tree/tree_radial.tcl
@@ -0,0 +1,35 @@
+lappend auto_path [file dirname [file dirname [file dirname [file dirname [file normalize [info script]]]]]]
+
+# source all.tcl
+if {[catch {package present ticklecharts}]} {package require ticklecharts}
+
+try {
+    # https://wiki.tcl-lang.org/page/HTTPS
+    #
+    package require http 2
+    package require tls 1.7
+    package require json
+
+    http::register https 443 [list ::tls::socket -autoservername true]
+    set token [http::geturl https://raw.githubusercontent.com/apache/echarts-examples/gh-pages/public/data/asset/data/flare.json]
+
+    set htmldata [::http::data $token]
+    set datajson [json::json2dict $htmldata]
+
+    set chart [ticklecharts::chart new]
+
+    $chart SetOptions -tooltip {trigger "item" triggerOn "mousemove"}
+                
+    $chart AddTreeSeries -top "18%" -bottom "14%" -layout "radial" -symbol "emptyCircle" -symbolSize 7 -initialTreeDepth 3 \
+                         -emphasis {focus "descendant"} \
+                         -data [list $datajson]
+
+    set fbasename [file rootname [file tail [info script]]]
+    set dirname [file dirname [info script]]
+
+    $chart Render -outfile [file join $dirname $fbasename.html] -title $fbasename -width 1500px -height 1500px
+
+} on error {result options} {
+    puts stderr "[info script] : $result"
+}
+
