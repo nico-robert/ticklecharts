7d5e0d333d7e9bff9098fc6cccbc71da1ccf7c57
nico-robert <nicolas.robert.19000@gmail.com>
2022-12-02 08:33:49 +0100

    Bump to 'v2.1.0' for echarts-wordcloud.js examples

--- examples/layout/bar_wordcloud.tcl
+++ examples/layout/bar_wordcloud.tcl
@@ -1,5 +1,8 @@
 lappend auto_path [file dirname [file dirname [file dirname [file dirname [file normalize [info script]]]]]]
 
+# v1.0 : Initial example
+# v2.0 : bump to 'v2.1.0' echarts-wordcloud
+
 proc fakerRandomValue {{min 10} {max 1000}} {
 
     set range [expr {$max - $min}]
@@ -95,7 +98,7 @@ set wc [ticklecharts::chart new]
 
 # Add js script
 set header [ticklecharts::jsfunc new {
-                        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/echarts-wordcloud@2.0.0/dist/echarts-wordcloud.min.js"></script>
+                        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/echarts-wordcloud@2.1.0/dist/echarts-wordcloud.min.js"></script>
                     } -header
             ]
 # callback color wordCloud
