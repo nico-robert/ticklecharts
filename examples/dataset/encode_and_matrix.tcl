cee078b86671fb5578077e8b118eb0c011d36b03
nico-robert <nicolas.robert.19000@gmail.com>
2023-05-10 19:38:46 +0200

    Update dataset example (v3.1.2) - Set new 'Add' method for chart series. - Adds a 'edict' class for dataset.dimension property.

--- examples/dataset/encode_and_matrix.tcl
+++ examples/dataset/encode_and_matrix.tcl
@@ -3,6 +3,10 @@ lappend auto_path [file dirname [file dirname [file dirname [file dirname [file 
 # v1.0 : Initial example
 # v2.0 : destroy layout class (problem source all.tcl...)
 # v3.0 : re-working 'dataset' class should be a list of list...
+# v4.0 : Set new 'Add' method for chart series + adds a 'edict' class for dataset.dimension property (3.1.3).
+#        Note : v3.0 version > Since some versions this is no longer quite true, it can be a simple list.
+#        Note : Add***Series will be deleted in the next major release, 
+#               in favor of this writing. (see formatter property + 'Add' method below)
 
 # source all.tcl
 if {[catch {package present ticklecharts}]} {package require ticklecharts}
@@ -23,43 +27,61 @@ try {
     set sizeValue "57%"
     set symbolSize 2.5
 
-    set dimensions {"Income" "Life Expectancy" "Population" "Country" {name "Year" type "ordinal"}}
+    # Adds eDict class (only for test)
+    # Note : Values are arbitrary, that does not matter on the graphic result.
+    set dimensions [list "Income" "Life Expectancy" "Population" "Country" \
+                         {name "Year" type "ordinal"} \
+                         [new edict {name "dummy" type "number"}]]
 
     # dataset class
-    set dset [ticklecharts::dataset new [list [list -dimensions $dimensions -source $datajson]]]
+    set dset [ticklecharts::dataset new [list -dimensions $dimensions -source $datajson]]
 
     # layout
     set layout [ticklecharts::Gridlayout new]
     $layout SetGlobalOptions -tooltip {} -toolbox {left "center" feature {dataZoom {}}} -dataset $dset
 
+    # new scatter
     set scatter1 [ticklecharts::chart new]
 
     $scatter1 Xaxis -type "value" -name "Income" -axisLabel {rotate 50 interval 0}
     $scatter1 Yaxis -type "value" -name "Life Expectancys"
-    $scatter1 AddScatterSeries -name "Scatter 1" -symbolSize $symbolSize -encode [list x "Income" y "Life Expectancy" tooltip [list {0 1 2 3 4}]]
 
+    $scatter1 Add "scatterSeries" -name "Scatter 1" \
+                                  -symbolSize $symbolSize \
+                                  -encode [list x "Income" y "Life Expectancy" tooltip [list {0 1 2 3 4}]]
+    
+    # new scatter
     set scatter2 [ticklecharts::chart new]
 
     $scatter2 Xaxis -type "category" -name "Country" -boundaryGap "false" -axisLabel {rotate 50 interval 0}
     $scatter2 Yaxis -type "value" -name "Income"
-    $scatter2 AddScatterSeries -name "Scatter 2" -symbolSize $symbolSize -encode [list x "Country" y "Income" tooltip [list {0 1 2 3 4}]]
 
+    $scatter2 Add "scatterSeries" -name "Scatter 2" \
+                                  -symbolSize $symbolSize \
+                                  -encode [list x "Country" y "Income" tooltip [list {0 1 2 3 4}]]
+    
+    # new scatter
     set scatter3 [ticklecharts::chart new]
 
     $scatter3 Xaxis -type "value" -name "Income" -axisLabel {rotate 50 interval 0}
     $scatter3 Yaxis -type "value" -name "Population"
-    $scatter3 AddScatterSeries -name "Scatter 3" -symbolSize $symbolSize -encode [list x "Income" y "Population" tooltip [list {0 1 2 3 4}]]
-
+    $scatter3 Add "scatterSeries" -name "Scatter 3" \
+                                  -symbolSize $symbolSize \
+                                  -encode [list x "Income" y "Population" tooltip [list {0 1 2 3 4}]]
+    
+    # new scatter
     set scatter4 [ticklecharts::chart new]
 
     $scatter4 Xaxis -type "value" -name "Life Expectancy" -axisLabel {rotate 50 interval 0}
     $scatter4 Yaxis -type "value" -name "Population"
-    $scatter4 AddScatterSeries -name "Scatter 4" -symbolSize $symbolSize -encode [list x "Life Expectancy" y "Population" tooltip [list {0 1 2 3 4}]]
+    $scatter4 Add "scatterSeries" -name "Scatter 4" \
+                                  -symbolSize $symbolSize \
+                                  -encode [list x "Life Expectancy" y "Population" tooltip [list {0 1 2 3 4}]]
 
     $layout Add $scatter1 -right $sizeValue -bottom $sizeValue
-    $layout Add $scatter2 -left $sizeValue -bottom $sizeValue
+    $layout Add $scatter2 -left  $sizeValue -bottom $sizeValue
     $layout Add $scatter3 -right $sizeValue -top $sizeValue
-    $layout Add $scatter4 -left $sizeValue -top $sizeValue
+    $layout Add $scatter4 -left  $sizeValue -top $sizeValue
 
     set fbasename [file rootname [file tail [info script]]]
     set dirname [file dirname [info script]]
