637cb65274f6c22e760c258d29ef711cbd95049b
nico-robert <nicolas.robert.19000@gmail.com>
2023-07-07 13:12:11 +0200

    Test Render's arguments with spaces :  - Bug introduced with version 3.1.2

--- examples/line/line_basic_chart.tcl
+++ examples/line/line_basic_chart.tcl
@@ -3,6 +3,7 @@ lappend auto_path [file dirname [file dirname [file dirname [file dirname [file 
 # v1.0 : Initial example
 # v2.0 : Replace 'render' method by 'Render' (Note the first letter in capital letter...)
 # v3.0 : Update example with the new 'Add' method for chart series.
+# v4.0 : Test Render's arguments with spaces (bug introduced with version 3.1.2)
 
 # source all.tcl
 if {[catch {package present ticklecharts}]} {package require ticklecharts}
@@ -16,4 +17,5 @@ $chart Add "lineSeries" -data [list {150 230 224 218 135 147 260}]
 set fbasename [file rootname [file tail [info script]]]
 set dirname [file dirname [info script]]
 
-$chart Render -outfile [file join $dirname $fbasename.html] -title $fbasename
+$chart Render -title "Magic ticklEcharts..." \
+              -outfile [file join $dirname $fbasename.html]
